69b2295d126776b16228c1fb5a1e7aa70ee2a96e
John Zhou <zhou9865@gmail.com>
2024-09-28 16:13:23 -0700

    setup color palettes and text

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bap: {
+          "brown-100": "#C5C19D", 
+          "brown-400": "#1C090A", // background color
+          "red-200": "#AF272F", 
+          "red-800": "#491014" // calendar text color, used nowhere else
+        },
+      },
+    },
   },
   plugins: [],
 };
