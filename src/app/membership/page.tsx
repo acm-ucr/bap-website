047c409883de3f3642c10768fceb5b2d25892e9f
John Zhou <zhou9865@gmail.com>
2024-09-28 17:52:00 -0700

    added membership page setup

---
+++ src/app/membership/page.tsx
@@ -0,0 +1,11 @@
+import Title from "@/components/Title";
+
+const Membership = () => {
+  return (
+    <div className="flex w-screen flex-col items-center">
+      <Title />
+    </div>
+  );
+};
+
+export default Membership;
