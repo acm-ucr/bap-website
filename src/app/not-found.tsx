1d410af9bc1f7043bc37ed3168df5cac9fe510c8
prajeshElEvEn <prajesh.eleven118@gmail.com>
2024-09-30 13:47:37 +0530

    added not-found.tsx page code

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,18 @@
+import Link from "next/link";
+
 const NotFound = () => {
-  return <div>404 Page Not Found</div>;
+  return (
+    <div className="flex flex-grow flex-col items-center justify-center gap-4">
+      <div className="flex text-6xl font-bold md:text-9xl">404</div>
+      <div className="flex text-2xl font-bold md:text-4xl">Page Not Found</div>
+
+      <Link className="mt-6" href="/">
+        <button className="rounded border px-4 py-2 font-bold">
+          Back to Home
+        </button>
+      </Link>
+    </div>
+  );
 };
 
 export default NotFound;
