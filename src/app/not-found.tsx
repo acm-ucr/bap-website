3507fe2a8915ab7ae1ac9a1fb3842874ce179477
amarajaRW <amaraja.v@randomwalk.ai>
2024-09-30 15:59:39 +0530

    fix: add 404 page not found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,15 +1,17 @@
 import Link from "next/link";
 const NotFound = () => {
   return (
-    <div className="flex-1 justify-center items-center flex flex-col">
-      <div className="flex text-red-400 text-8xl font-bold py-2">404</div>
-      <div className="flex text-rishi-black font-bold text-2xl py-2">
+    <div className="flex flex-1 flex-col items-center justify-center">
+      <div className="flex py-2 text-8xl font-bold text-red-400">404</div>
+      <div className="text-rishi-black flex py-2 text-2xl font-bold">
         Page Not Found
       </div>
 
       <Link href="/">
         <div className="flex py-4">
-          <button className="border border-white rounded-md px-5 py-1 transform transition-transform hover:scale-105">Back to Home</button>
+          <button className="transform rounded-md border border-white px-5 py-1 transition-transform hover:scale-105">
+            Back to Home
+          </button>
         </div>
       </Link>
     </div>
