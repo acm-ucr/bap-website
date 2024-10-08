043ab805878a0d19a648c454826e67dad46ed9b2
theo <theobfernandez@gmail.com>
2024-10-07 20:47:10 -0700

    Add title

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,4 +1,12 @@
-const Title = () => {
-  return <div className="">Title</div>;
+type props = {
+  title: string;
+}
+const Title = ({title}: props) => {
+  return (
+    <div className="flex flex-col">
+      <div className="">{title}</div>
+      <div className="bg-bap-red-200 pt-0.5"></div>
+    </div>
+  );
 };
 export default Title;
