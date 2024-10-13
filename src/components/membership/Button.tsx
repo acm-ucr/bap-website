616a1ed4f656878f589d8e00ee0f960ad8eba0e8
Rebecca-J7 <rjenn003@ucr.edu>
2024-10-12 17:50:13 -0700

    Added text to Button.tsx and created Button.css file

--- src/components/membership/Button.tsx
+++ src/components/membership/Button.tsx
@@ -1,5 +1,12 @@
-const Button = () => {
-  return <div>Button</div>;
+import React from 'react';
+import './Button.css';
+
+const Button: React.FC = () => {
+  return (
+    <div className="Button">
+      MEMBERSHIP FORM
+    </div>;
+  );
 };
 
 export default Button;
