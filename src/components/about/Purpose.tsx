0cc11834250ca6a4822f7f7fa182f953d7117c90
daniyalbhat34 <daniyalbhat34@gmail.com>
2024-10-14 01:22:26 -0700

    <Done with PurposeText>

--- src/components/about/Purpose.tsx
+++ src/components/about/Purpose.tsx
@@ -1,7 +1,9 @@
 const Purpose = () => {
   return (
     <div className="">
-      <p>Purpose Section</p>
+      <p style={{ fontSize: '2em', marginBottom: '20px' }}>OUR PURPOSE <br/></p>
+      <p>Beta Alpha Psi's purpose is to cultivate  professional and personal growth,<br />
+      academic excellence, and a strong sense of community among its members.</p>
     </div>
   );
 };
