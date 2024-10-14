aac913f8dce157b30ee9f9ec5d914e759eeaea5e
Moses Avila <mosesavila9@gmail.com>
2024-10-13 21:38:34 -0700

    Prettier formatted the code

--- src/components/home/Mission.tsx
+++ src/components/home/Mission.tsx
@@ -1,11 +1,12 @@
 const Mission = () => {
   return (
-    <div className="bg-[#b35644] p-10 rounded-lg text-center max-w-lg mx-auto">
-      <h1 className="text-white text-3xl font-bold mb-6">MISSION STATEMENT</h1>
-      <p className="text-white text-lg leading-relaxed">
-        It provides opportunities for service, professional development, and interaction among its members and financial professionals. 
-        Additionally, Beta Alpha Psi fosters lifelong ethical, social, and public responsibilities, shaping its members into leaders 
-        in their fields.
+    <div className="mx-auto max-w-lg rounded-lg bg-[#b35644] p-10 text-center">
+      <h1 className="mb-6 text-3xl font-bold text-white">MISSION STATEMENT</h1>
+      <p className="text-lg leading-relaxed text-white">
+        It provides opportunities for service, professional development, and
+        interaction among its members and financial professionals. Additionally,
+        Beta Alpha Psi fosters lifelong ethical, social, and public
+        responsibilities, shaping its members into leaders in their fields.
       </p>
     </div>
   );
