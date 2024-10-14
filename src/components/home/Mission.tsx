c36d4183e3f1b3590f6cbe60b91dd88878b76af7
Moses Avila <mosesavila9@gmail.com>
2024-10-13 22:00:28 -0700

    Adjusted the container sizing and look

--- src/components/home/Mission.tsx
+++ src/components/home/Mission.tsx
@@ -1,6 +1,6 @@
 const Mission = () => {
   return (
-    <div className="mx-auto max-w-lg rounded-lg bg-[#a12424] p-10 text-center">
+    <div className="mx-auto max-w-lg bg-[#a12424] p-10 text-center">
       <h1 className="mb-6 text-3xl font-normal text-white">
         MISSION STATEMENT
       </h1>
