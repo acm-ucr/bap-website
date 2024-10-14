7b18825df09e5abfabeda45f937693f1bb7f3451
Rebecca-J7 <rjenn003@ucr.edu>
2024-10-14 14:17:13 -0700

    Changed Button file to tailwind css format

--- src/components/membership/Button.tsx
+++ src/components/membership/Button.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import './Button.css';
+import React from "react";
 
 const Button: React.FC = () => {
   return (
-    <div className="Button">
+    <div className="rounded-lg bg-red-700 px-6 py-3 font-bold text-white transition hover:scale-105 hover:bg-red-800 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 active:scale-95">
       MEMBERSHIP FORM
-    </div>;
+    </div>
   );
 };
 
