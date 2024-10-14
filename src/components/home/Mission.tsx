2fba18f3d180179db83757904ce7aed45d75e556
Moses Avila <mosesavila9@gmail.com>
2024-10-13 21:36:45 -0700

    Created BAP mission statement

--- src/components/home/Mission.tsx
+++ src/components/home/Mission.tsx
@@ -2,7 +2,11 @@ const Mission = () => {
   return (
     <div className="bg-[#b35644] p-10 rounded-lg text-center max-w-lg mx-auto">
       <h1 className="text-white text-3xl font-bold mb-6">MISSION STATEMENT</h1>
-      <p className=""></p>
+      <p className="text-white text-lg leading-relaxed">
+        It provides opportunities for service, professional development, and interaction among its members and financial professionals. 
+        Additionally, Beta Alpha Psi fosters lifelong ethical, social, and public responsibilities, shaping its members into leaders 
+        in their fields.
+      </p>
     </div>
   );
 };
