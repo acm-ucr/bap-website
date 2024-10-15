74fd73affa2d3711d25f1c9d6b4d59eb53841f0b
adhvait5 <adhvait2004@gmail.com>
2024-10-14 20:25:41 -0700

    update 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,17 +1,19 @@
-import Button from "@/app/Button.jsx";
 import Link from "next/link";
 
 const NotFound = () => {
+
   return (
-    <div className="flex flex-1 flex-col items-center justify-center">
-      <div className="flex py-2 text-9xl font-bold text-red-200">404</div>
-      <div className="flex py-2 text-4xl font-bold text-red-200">
+    <div className="flex-1 justify-center items-center flex flex-col">
+      <div className="flex text-bap-red-200 text-9xl font-bold py-2">404</div>
+      <div className="flex text-bap-red-200 font-bold text-4xl py-2">
         Page Not Found
       </div>
 
       <Link href="/">
         <div className="flex py-4">
-          <Button buttonText={"Back to Home"} />
+        <button className="bg-bap-brown-400 border-bap-brown-100 text-bap-red-200 font-urbanist hover:bg-bap-orange rounded rounded-md border-2 px-4 px-6 text-2xl duration-300 hover:scale-110"> 
+        Back to Home
+        </button>
         </div>
       </Link>
     </div>
