2cc6025d32a95bf392d0ada32aeb991925cdd4d1
Roseanne <shildt.roseanne@gmail.com>
2024-10-14 20:28:10 -0700

    Add

--- src/components/home/Membership.tsx
+++ src/components/home/Membership.tsx
@@ -1,12 +1,20 @@
 const Membership = () => {
   return (
-    <div className="" 
-    style={{display: 'block', backgroundColor: '#AF272F',
-      textAlign: 'center', width: '400px', height: '105px', font: 'status-bar'
-    }}>
-      <p style={{fontSize: '3em', margin: '5px 0 10px 0', }}> MEMBERSHIP</p>
-      <p style={{fontSize: '1.05em'}}> 
-      Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit, Sed Do Eiusmod Tempor Incididunt Ut Labore Et Dolore Magna Aliqua
+    <div
+      className=""
+      style={{
+        display: "block",
+        backgroundColor: "#AF272F",
+        textAlign: "center",
+        width: "375px",
+        height: "105px",
+        font: "status-bar",
+      }}
+    >
+      <p style={{ fontSize: "3em", margin: "5px 0 10px 0" }}> MEMBERSHIP</p>
+      <p style={{ fontSize: "1em" }}>
+        Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit, Sed Do Eiusmod
+        Tempor Incididunt Ut Labore Et Dolore Magna Aliqua
       </p>
     </div>
   );
