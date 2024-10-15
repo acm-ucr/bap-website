dcf0b8a8f175a708206cb30f19d1d314ee02ec6a
SGH-SS <ssrin043@ucr.edu>
2024-10-14 20:32:59 -0700

    removed extra comments and changed to flexbox for arrows

--- src/components/home/EventGallery.tsx
+++ src/components/home/EventGallery.tsx
@@ -1,17 +1,19 @@
 "use client";
-import Image from "next/image";
+
+import Image from 'next/image';
 
 const EventGallery = () => {
   return (
-    <div className="select-none flex-col">
-      <h2 className="text-center text-2xl font-semibold">Event Gallery</h2>
-
-      <div className="pt-4" />
-
-      {/* Main Event Image with Button Overlays */}
+    <div className="pt-4">
       <div className="relative flex justify-center items-center">
-        {/* Main Image */}
-        <div className="relative">
+        <div className="relative flex justify-between items-center w-full">
+          <button
+            className="bg-transparent border-none"
+            style={{ width: '500px', height: '500px'}}
+            onClick={() => alert("Left arrow clicked")}
+          >
+          </button>
+
           <Image
             src="/ucr_event.webp"
             alt="UCR Event"
@@ -19,23 +21,12 @@ const EventGallery = () => {
             height={1000}
             className="object-cover"
           />
-          
-          {/* Left Button */}
-          <button
-            className="absolute left-15 top-1/2 transform -translate-y-1/2 bg-transparent border-none"
-            style={{ width: '130px', height: '130px' }}
-            onClick={() => alert("Left arrow clicked")}
-          >
-            <span className="sr-only">Previous Event</span>
-          </button>
 
-          {/* Right Button */}
           <button
-            className="absolute right-10 top-1/2 transform -translate-y-1/2 bg-transparent border-none"
-            style={{ width: '150px', height: '150px' }}
+            className="bg-transparent border-none"
+            style={{ width: '500px', height: '500px', marginLeft: '-50px' }}
             onClick={() => alert("Right arrow clicked")}
           >
-            <span className="sr-only">Next Event</span>
           </button>
         </div>
       </div>
