f40df69d419098024daa616393b1eccaec3335a4
Roseanne <shildt.roseanne@gmail.com>
2024-10-14 21:18:55 -0700

    add

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,11 +1,13 @@
 import Title from "@/components/Title";
 import Calendar from "@/components/events/Calendar";
 import EventCard from "@/components/events/EventCard";
+import Landing from "@/components/home/Landing";
 
 const Events = () => {
   return (
     <div className="flex w-screen flex-col items-center">
       <Title />
+      <Landing />
       <EventCard />
       <EventCard />
       <EventCard />
