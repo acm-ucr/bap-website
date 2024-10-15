334df74215d616584c30fa0e2125b362d35db2c7
daniyalbhat34 <daniyalbhat34@gmail.com>
2024-10-14 21:16:58 -0700

    about section

--- src/components/about/Purpose.tsx
+++ src/components/about/Purpose.tsx
@@ -1,7 +1,15 @@
 const Purpose = () => {
   return (
-    <div className="">
-      <p>Purpose Section</p>
+    <div className="flex flex-col">
+      <p className="text-3xl">
+        OUR PURPOSE <br />
+      </p>
+      <p>
+        Beta Alpha Psi's purpose is to cultivate professional and personal
+        growth,
+        <br />
+        academic excellence, and a strong sense of community among its members.
+      </p>
     </div>
   );
 };
