f1698891ae3717e209f5965c989d2146d3786330
Moses Avila <mosesavila9@gmail.com>
2024-10-14 17:51:47 -0700

    Fixed formatting issues

--- src/components/home/Mission.tsx
+++ src/components/home/Mission.tsx
@@ -1,9 +1,7 @@
 const Mission = () => {
   return (
     <div className="mx-auto max-w-lg bg-[#a12424] px-10 pb-10 pt-10 text-center">
-      <h1 className="mb-6 text-4xl font-thin text-white">
-        MISSION STATEMENT
-      </h1>
+      <h1 className="mb-6 text-4xl font-thin text-white">MISSION STATEMENT</h1>
       <p className="text-lg leading-relaxed text-white">
         It provides opportunities for service, professional development, and
         interaction among its members and financial professionals. Additionally,
