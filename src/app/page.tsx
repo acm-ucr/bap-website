f8db99dfc0bee20f7c95e4e3c6cd09480c593158
Roseanne <shildt.roseanne@gmail.com>
2024-10-14 21:10:41 -0700

    add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,6 @@ const Home = () => {
     <div className="flex h-screen w-screen items-center justify-center">
       <Landing />
 
-     
       <Membership />
     </div>
   );
--- src/components/home/Membership.tsx
+++ src/components/home/Membership.tsx
@@ -1,8 +1,6 @@
 const Membership = () => {
   return (
-    <div
-      className="flex-col bg-bap-red-200 text-center py-3 px-3 "
-    >
+    <div className="flex-col bg-bap-red-200 px-3 py-3 text-center">
       <p className="text-2xl"> MEMBERSHIP</p>
       <p className="text-xs">
         Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit, Sed Do Eiusmod
