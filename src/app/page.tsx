b7fdd2d38a70ce87a8d5fc57c4e7c98b372f93a7
Roseanne <shildt.roseanne@gmail.com>
2024-10-14 21:14:13 -0700

    add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,10 @@ const Home = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
       <Landing />
-
+      <Mission />
+      <EventGallery />
       <Membership />
+      <Professional />
     </div>
   );
 };
