20c4a1ec12acdc983b1c5e70075c29acc0a8be9c
Mostafa Elmallah <mmmelmallah@gmail.com>
2024-10-14 20:46:48 -0700

    did final touch should be good now

--- src/components/home/Professionals.tsx
+++ src/components/home/Professionals.tsx
@@ -1,6 +1,6 @@
 const Professional = () => {
   return (
-    <div className="bg-bap-brown-100 px-2 font-mono">
+    <div className="bg-bap-brown-100 bg-opacity-40 px-6 py-2 font mono" >
       <p>FEATURED PROFESSIONALS</p>
     </div>
   );
