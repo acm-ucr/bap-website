c40aa45b713b45fc2616ce9bcb0ebe48a73357ab
adhvait5 <adhvait2004@gmail.com>
2024-10-14 20:41:27 -0700

    update 404 page underline

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -8,7 +8,7 @@ const NotFound = () => {
         Page Not Found
       </div>
 
-      <Link className ="no-underline" href="/">
+      <Link className="no-underline" href="/">
         <div className="flex py-4">
           <button className="font-urbanist hover:bg-bap-orange rounded border-2 border-bap-brown-100 bg-bap-brown-400 px-6 text-2xl text-bap-red-200 duration-300 hover:scale-110">
             Back to Home
