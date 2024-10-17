760f77b7fbe8835c80e2d0fd25b86f0ab15172dd
Roseanne <shildt.roseanne@gmail.com>
2024-10-17 12:22:30 -0700

    add button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
 import EventGallery from "@/components/home/EventGallery";
 import Landing from "@/components/home/Landing";
 import Membership from "@/components/home/Membership";
-import Mission from "@/components/home/Mission";
+//import Mission from "@/components/home/Mission";
 import Professional from "@/components/home/Professionals";
 
 const Home = () => {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
       <Landing />
-      <Mission />
       <EventGallery />
       <Membership />
       <Professional />
@@ -17,3 +16,7 @@ const Home = () => {
 };
 
 export default Home;
+
+/*
+<Mission />
+*/
