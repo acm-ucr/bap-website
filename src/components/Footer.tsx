5e905174ca4ead29eed9bcc1ba418cde48b41b3a
Marco Navarro <duvalincus@gmail.com>
2024-10-17 21:35:05 -0700

    smaller text, larger padding

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,23 +7,23 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <div className="font-bap flex w-full bg-bap-brown-100">
-      <div className="my-6 flex flex-1 flex-row justify-between px-16">
+      <div className="my-12 flex flex-1 flex-row justify-between px-12">
         <div className="flex w-1/2 flex-row items-end">
-          <div className="flex flex-row items-center pl-5">
-            <Image src={bap} className="mr-4 w-1/6 text-3xl" alt="bap logo" />
-            <p className="mb-0 text-4xl">BETA ALPHA PSI</p>
+          <div className="ml-5 flex flex-row items-center gap-2">
+            <Image src={bap} className="w-[9%]" alt="bap logo" />
+            <p className="mb-0 text-3xl">BETA ALPHA PSI</p>
           </div>
         </div>
-        <div className="flex flex-col gap-2 text-right">
-          <p className="mb-0 text-3xl">Contact Us</p>
+        <div className="flex flex-col justify-center gap-1 text-right">
+          <p className="mb-0 text-xl">Contact Us</p>
           <Link
             href="mailto:betaalphapsi@gmail.com"
             target="_blank"
-            className="mb-0 text-2xl hover:text-gray-200"
+            className="mb-0 text-lg hover:text-gray-200"
           >
             <p>betaalphapsi@gmail.com</p>
           </Link>
-          <div className="flex flex-row justify-end gap-3 text-4xl">
+          <div className="flex flex-row justify-end gap-3 text-3xl">
             <FaInstagram />
             <RxDiscordLogo />
             <FaLinkedin />
