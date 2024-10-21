6cc1d001c1a6e0057e5ae4045d84efc05600fcfc
Sathya <sathya.srinivasan11@gmail.com>
2024-10-21 12:31:48 -0700

    ran formatting thru prettier

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -1,15 +1,13 @@
-import React from 'react';
-import BoardMemberCard from '@/components/board/BoardMemberCard';
+import React from "react";
+import BoardMemberCard from "@/components/board/BoardMemberCard";
 
 const BoardPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 p-4">
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <div className="grid grid-cols-1 gap-8 p-4 sm:grid-cols-2 lg:grid-cols-3">
         <BoardMemberCard title="Faculty Advisor" name="Professor John Doe" />
         <BoardMemberCard title="President" name="Jane Smith" />
         <BoardMemberCard title="Vice President" name="Alice Johnson" />
-
       </div>
     </div>
   );
--- src/components/board/BoardMemberCard.tsx
+++ src/components/board/BoardMemberCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 type Props = {
   title: string;
@@ -7,17 +7,19 @@ type Props = {
 
 const BoardMemberCard: React.FC<Props> = ({ title, name }) => {
   return (
-    <button style={{
-      backgroundColor: '#A71D24',
-      color: '#fff',
-      padding: '10px',
-      borderRadius: '5px',
-      textAlign: 'center',
-      width: '200px',
-      border: 'none',
-      cursor: 'pointer'
-    }}>
-      <div style={{ fontWeight: 'bold' }}>{title}</div>
+    <button
+      style={{
+        backgroundColor: "#A71D24",
+        color: "#fff",
+        padding: "10px",
+        borderRadius: "5px",
+        textAlign: "center",
+        width: "200px",
+        border: "none",
+        cursor: "pointer",
+      }}
+    >
+      <div style={{ fontWeight: "bold" }}>{title}</div>
       <div>{name}</div>
     </button>
   );
