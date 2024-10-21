625b226993e7ba67101586182401725d990862b5
adhvait5 <adhvait2004@gmail.com>
2024-10-20 17:44:22 -0700

    Added purpose icons

--- src/components/about/Purpose.tsx
+++ src/components/about/Purpose.tsx
@@ -10,6 +10,40 @@ const Purpose = () => {
         <br />
         academic excellence, and a strong sense of community among its members.
       </p>
+
+      <div className="max-w-10xl row-auto mx-auto grid grid-cols-1 gap-x-20 bg-bap-brown-100 md:grid-cols-4">
+        <div className="flex flex-col items-center">
+          <img src="diploma.webp" className="l-40 w-40"></img>
+          <span className="text-center text-2xl">
+            ACADEMIC <br />
+            EXCELLENCE
+          </span>
+        </div>
+
+        <div className="flex flex-col items-center">
+          <img src="thinking.webp" className="l-40 w-40"></img>
+          <span className="text-center text-2xl">
+            PERSONAL <br />
+            DEVELOPMENT
+          </span>
+        </div>
+
+        <div className="flex flex-col items-center">
+          <img src="handshake.webp" className="l-40 w-40"></img>
+          <span className="text-center text-2xl">
+            PROFESSIONAL <br />
+            DEVELOPMENT
+          </span>
+        </div>
+
+        <div className="flex flex-col items-center">
+          <img src="discussion.webp" className="l-40 w-40"></img>
+          <span className="text-center text-2xl">
+            SENSE OF <br />
+            COMMUNITY
+          </span>
+        </div>
+      </div>
     </div>
   );
 };
