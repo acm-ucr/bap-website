78455ae47d41edede459c069e071505c3ae479e5
Moses Avila <mosesavila9@gmail.com>
2024-10-21 14:19:27 -0700

    Added club image from public folder

--- src/components/home/Mission.tsx
+++ src/components/home/Mission.tsx
@@ -1,13 +1,28 @@
 const Mission = () => {
   return (
-    <div className="mx-auto max-w-lg bg-[#a12424] px-10 pb-10 pt-10 text-center">
-      <h1 className="mb-6 text-4xl font-thin text-white">MISSION STATEMENT</h1>
-      <p className="text-lg leading-relaxed text-white">
-        It provides opportunities for service, professional development, and
-        interaction among its members and financial professionals. Additionally,
-        Beta Alpha Psi fosters lifelong ethical, social, and public
-        responsibilities, shaping its members into leaders in their fields.
-      </p>
+    <div className="relative h-full w-full">
+      <img
+        src="/clubimage2.webp"
+        alt="Club Image"
+        className="h-full w-full object-cover"
+      />
+
+      <div className="absolute left-0 top-0 max-w-lg bg-[#a12424] px-10 pb-20 pt-10 text-center">
+        <h1 className="mb-6 text-4xl font-thin text-white">
+          MISSION STATEMENT
+        </h1>
+        <p className="text-lg leading-relaxed text-white">
+          It provides opportunities for service, professional development, and
+          interaction among its members and financial professionals.
+          Additionally, Beta Alpha Psi fosters lifelong ethical, social, and
+          public responsibilities, shaping its members into leaders in their
+          fields.
+        </p>
+      </div>
+
+      <div className="">
+        “Knowledge Is The Key To Opening The Doors Of The Financial World”
+      </div>
     </div>
   );
 };
