c0a6b0edf2ecc102d9ec822613a890e45edbdffd
adhvait5 <adhvait2004@gmail.com>
2024-10-21 20:34:26 -0700

    updated purpose icons

--- src/components/about/Purpose.tsx
+++ src/components/about/Purpose.tsx
@@ -1,3 +1,9 @@
+import Image from "next/image";
+import diploma from "@/public/diploma.webp";
+import thinking from "@/public/thinking.webp";
+import handshake from "@/public/handshake.webp";
+import discussion from "@/public/discussion.webp";
+
 const Purpose = () => {
   return (
     <div className="flex flex-col">
@@ -11,39 +17,42 @@ const Purpose = () => {
         academic excellence, and a strong sense of community among its members.
       </p>
 
-      <div className="max-w-10xl row-auto mx-auto grid grid-cols-1 gap-x-20 bg-bap-brown-100 md:grid-cols-4">
-        <div className="flex flex-col items-center">
-          <img src="diploma.webp" className="l-40 w-40"></img>
+      
+      <div className = "flex flex-row bg-bap-brown-100 bg-opacity-50 place-items-center p-3">
+        <div className="flex flex-col justify-center items-center px-3">
+          <Image src={diploma} className="l-40 w-40"></Image>
           <span className="text-center text-2xl">
             ACADEMIC <br />
             EXCELLENCE
           </span>
         </div>
 
-        <div className="flex flex-col items-center">
-          <img src="thinking.webp" className="l-40 w-40"></img>
+        <div className="flex flex-col items-center justify-center px-3">
+          <Image src= {thinking} className="l-40 w-40"></Image>
           <span className="text-center text-2xl">
             PERSONAL <br />
             DEVELOPMENT
           </span>
         </div>
 
-        <div className="flex flex-col items-center">
-          <img src="handshake.webp" className="l-40 w-40"></img>
+        <div className="flex flex-col items-center justify-center px-3">
+          <Image src={handshake} className="l-40 w-40"></Image>
           <span className="text-center text-2xl">
             PROFESSIONAL <br />
             DEVELOPMENT
           </span>
         </div>
 
-        <div className="flex flex-col items-center">
-          <img src="discussion.webp" className="l-40 w-40"></img>
+        <div className="flex flex-col items-center justify-center px-3">
+          <Image src= {discussion} className="l-40 w-40"></Image>
           <span className="text-center text-2xl">
             SENSE OF <br />
             COMMUNITY
           </span>
         </div>
+
       </div>
+     
     </div>
   );
 };
