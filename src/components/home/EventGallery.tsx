36306c4e8f1d7ef3fa348e31424c6b98e3f446f4
Anthony Vuong <avuon032@xe-02.cs.ucr.edu>
2024-10-21 20:35:16 -0700

    fix

--- src/components/home/EventGallery.tsx
+++ src/components/home/EventGallery.tsx
@@ -1,7 +1,8 @@
 const EventGallery = () => {
   return (
-    <div className="whitespace-nowrap bg-red-800 p-3 text-white shadow-lg">
-      <p>Event Gallery</p>
+    <div className="whitespace-nowrap bg-red-800 p-3 text-white shadow-lg flex items-center justify-center">
+      <p className = "m-0">EVENT GALLERY</p>
+      
     </div>
   );
 };
