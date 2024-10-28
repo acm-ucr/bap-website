c9f59d9f392b0411fcd58faba43e8ba30849ac92
daniyalbhat34 <daniyalbhat34@gmail.com>
2024-10-28 01:53:00 -0700

    Completed Landing Box

--- src/components/home/Landing.tsx
+++ src/components/home/Landing.tsx
@@ -1,7 +1,14 @@
 const Landing = () => {
   return (
-    <div className="">
-      <p>Landing</p>
+    <div className="flex flex-col bg-gray-300 bg-opacity-50 pt-1 pb-1 pl-3 pr-3">
+      <p className="text-3xl font-thin leading-8">
+        Honors Organization for Financial Information <br /> Students and
+        Professionals at UCR
+      </p>
+      <p className="ml-64 mt-1 text-lg font-thin leading-tight">
+        Empowering Tomorrow's Leaders in Finance and <br /> Business Through
+        Excellence, Ethics, and Engagement
+      </p>
     </div>
   );
 };
