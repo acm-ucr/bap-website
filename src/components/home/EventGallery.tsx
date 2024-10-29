8500d184fd947af838535ce8daf97c09fea7e5e6
Marco Navarro <duvalincus@gmail.com>
2024-10-28 18:14:04 -0700

    add gallery, arrows, and half label

--- src/components/home/EventGallery.tsx
+++ src/components/home/EventGallery.tsx
@@ -1,7 +1,60 @@
+"use client";
+import Image from "next/image";
+import { SlArrowRight, SlArrowLeft } from "react-icons/sl";
+import clubimage1 from "@/public/clubimage1.webp";
+import clubimage2 from "@/public/clubimage2.webp";
+import clubimage3 from "@/public/clubimage3.webp";
+import { useState } from "react";
+
+const images = [clubimage1, clubimage2, clubimage3];
+
 const EventGallery = () => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const nextImage = () => {
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === images.length - 1 ? 0 : prevIndex + 1,
+    );
+  };
+
+  const prevImage = () => {
+    setCurrentImageIndex((prevIndex) =>
+      prevIndex === 0 ? images.length - 1 : prevIndex - 1,
+    );
+  };
+
   return (
-    <div className="flex items-center justify-center whitespace-nowrap bg-red-800 p-3 text-white shadow-lg">
-      <p className="m-0">EVENT GALLERY</p>
+    <div className="flex items-center justify-center whitespace-nowrap p-3 text-white shadow-lg">
+      <div className="flex w-3/4 flex-row items-center justify-center">
+        <div className="relative">
+          <div className="m-4">
+            <div className="relative">
+              <div className="absolute left-0 top-[50%] aspect-square opacity-80">
+                <SlArrowLeft
+                  size={50}
+                  className="cursor-pointer bg-red-700 p-2 text-xl text-white"
+                  onClick={prevImage}
+                />
+              </div>
+              <Image
+                src={images[currentImageIndex]}
+                alt="club image 1"
+                className="w-full"
+              />
+              <div className="absolute right-0 top-[48%] aspect-square opacity-80">
+                <SlArrowRight
+                  size={50}
+                  className="cursor-pointer bg-red-700 p-2 text-xl text-white"
+                  onClick={nextImage}
+                />
+              </div>
+            </div>
+          </div>
+          <p className="z-1 absolute right-0 top-0 m-0 bg-red-800 p-4 text-2xl shadow-2xl">
+            EVENT GALLERY
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
