776312e28f4181fb4bd1857036b55bda8ec03b2c
John Zhou <zhou9865@gmail.com>
2024-10-28 20:11:30 -0700

    fix

--- src/components/home/Landing.tsx
+++ src/components/home/Landing.tsx
@@ -2,13 +2,16 @@ const Landing = () => {
   return (
     <div className="flex flex-col bg-gray-300 bg-opacity-50 pb-1 pl-3 pr-3 pt-1">
       <p className="text-3xl font-thin leading-8">
-        Honors Organization for Financial Information <br /> Students and
-        Professionals at UCR
+        Honors Organization for Financial Information Students and Professionals
+        at UCR
       </p>
-      <p className="ml-64 mt-1 text-lg font-thin leading-tight">
-        Empowering Tomorrow's Leaders in Finance and <br /> Business Through
-        Excellence, Ethics, and Engagement
-      </p>
+
+      <div className="flex w-full flex-row justify-end border-4 border-red-500">
+        <p className="mt-1 w-[60%] border-4 border-green-500 text-lg font-thin leading-tight">
+          Empowering Tomorrow's Leaders in Finance and Business Through
+          Excellence, Ethics, and Engagement
+        </p>
+      </div>
     </div>
   );
 };
