83729038b31cc87fbab3fcbd387c8909ed76ba9b
Rebecca-J7 <rjenn003@ucr.edu>
2024-10-28 20:34:11 -0700

    Added margins and fixed responsive of Event Cards

--- src/components/events/EventCard.tsx
+++ src/components/events/EventCard.tsx
@@ -1,12 +1,13 @@
 const EventCard = () => {
   return (
-    // height might need to change due to possible larger descriptions
-    <div className="mx-4 flex h-auto flex-col justify-between rounded-md bg-bap-brown-100 bg-opacity-50 px-4 py-6 sm:mx-8 sm:px-8 sm:py-8 md:mx-12 lg:mx-48 lg:py-9 lg:pl-12 lg:pr-72">
-      <p className="text-3x1 mb-4 sm:text-4xl lg:text-5xl">WEEKLY MEETING</p>
-      <p className="mb-1 text-xl font-semibold text-bap-red-200 sm:text-2xl">
+    <div className="mx-4 my-10 flex h-auto w-[80%] flex-col justify-between rounded-md bg-bap-brown-100 bg-opacity-50 py-8 pl-10 sm:mx-8 md:mx-12 lg:mx-24">
+      <p className="text-3x1 mb-4 w-[80%] sm:text-4xl lg:text-5xl">
+        WEEKLY MEETING
+      </p>
+      <p className="mb-1 w-[80%] text-xl font-semibold text-bap-red-200 sm:text-2xl">
         August 14th @ 6:00pm - 7:00pm | Location
       </p>
-      <p className="text-lg sm:text-xl lg:text-2xl">
+      <p className="w-[80%] text-lg sm:text-xl lg:text-2xl">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua.
       </p>
