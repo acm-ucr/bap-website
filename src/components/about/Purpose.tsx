a83a174a80e93e72dd6fd39bd1e7088b6b273910
adhvait5 <adhvait2004@gmail.com>
2024-10-28 18:52:39 -0700

    added purpose icons dropdown

--- src/components/about/Purpose.tsx
+++ src/components/about/Purpose.tsx
@@ -15,32 +15,77 @@ const Purpose = () => {
         members.
       </p>
 
-      <div className="flex flex-row place-items-center bg-bap-brown-100 bg-opacity-50 p-3">
-        <div className="flex flex-col items-center justify-center px-3">
+      <div className="flex flex-row place-items-center justify-center overflow-hidden bg-bap-brown-100 bg-opacity-50 p-3">
+        <div
+          id="academic"
+          className="group/academic relative flex flex-col items-center justify-center px-3 hover:bg-bap-brown-100"
+        >
           <Image alt="diploma" src={diploma} className="l-40 w-40"></Image>
           <span className="text-center text-2xl">
             ACADEMIC <br />
             EXCELLENCE
           </span>
+
+          <div className="hidden flex-row bg-bap-red-200 group-hover/academic:flex group-hover/academic:bg-bap-red-200">
+            <ul>
+              <li>
+                <a className="block">
+                  By surrounding yourself with like-minded individuals, you will
+                  be able to engage in a supportive and motivating environment.
+                  Our GPA requirement of 3.2 helps maintain our professional
+                  reputation while encouraging members to sustain strong
+                  academic performance. We want to recognize members and reward
+                  academic excellence while inspiring all members to achieve
+                  their best.
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
 
-        <div className="flex flex-col items-center justify-center px-3">
+        <div
+          id="personal"
+          className="group/personal relative flex flex-col items-center justify-center px-3 hover:bg-bap-brown-100"
+        >
           <Image alt="thinking" src={thinking} className="l-40 w-40"></Image>
           <span className="text-center text-2xl">
             PERSONAL <br />
             DEVELOPMENT
           </span>
+
+          <div className="hidden flex-col group-hover/personal:flex group-hover/personal:bg-bap-red-200">
+            <a className="block">
+              We prioritize personal development as it forms the foundation for
+              professional success. The opportunities we offer will help you
+              refine your networking, communication, and leadership skills.
+            </a>
+          </div>
         </div>
 
-        <div className="flex flex-col items-center justify-center px-3">
+        <div
+          id="prof"
+          className="group/prof relative flex flex-col items-center justify-center px-3 hover:bg-bap-brown-100"
+        >
           <Image alt="handshake" src={handshake} className="l-40 w-40"></Image>
           <span className="text-center text-2xl">
             PROFESSIONAL <br />
             DEVELOPMENT
           </span>
+
+          <div className="hidden flex-col group-hover/prof:flex group-hover/prof:bg-bap-red-200">
+            <a className="block">
+              At BAP, we facilitate events with firms for our members to build
+              meaningful professional relationships with recruiters and
+              professionals. Our strong alumni network also offers valuable
+              support and guidance throughout your college and career journey.
+            </a>
+          </div>
         </div>
 
-        <div className="flex flex-col items-center justify-center px-3">
+        <div
+          id="sense"
+          className="group/sense relative flex flex-col items-center justify-center px-3 hover:bg-bap-brown-100"
+        >
           <Image
             alt="discussion"
             src={discussion}
@@ -50,6 +95,14 @@ const Purpose = () => {
             SENSE OF <br />
             COMMUNITY
           </span>
+
+          <div className="hidden flex-col group-hover/sense:flex group-hover/sense:bg-bap-red-200">
+            <a className="block">
+              BAP promotes participation in community service activities to give
+              back to the community, helping members develop a strong sense of
+              ethical, social, and public responsibility.
+            </a>
+          </div>
         </div>
       </div>
     </div>
