6b474377290b4e7df0403e477f767662f8083472
adhvait5 <adhvait2004@gmail.com>
2024-10-29 20:09:50 -0700

    uodated purpose icon dropdown

--- src/components/about/Purpose.tsx
+++ src/components/about/Purpose.tsx
@@ -1,10 +1,53 @@
+"use client";
 import Image from "next/image";
 import diploma from "@/public/diploma.webp";
 import thinking from "@/public/thinking.webp";
 import handshake from "@/public/handshake.webp";
 import discussion from "@/public/discussion.webp";
 
-const Purpose = () => {
+import { useState } from "react";
+
+const Purpose = ({}) => {
+  const [active, setActive] = useState(null);
+  console.log("current state", active);
+
+  const item = "theo";
+
+  const items = [
+    {
+      id: "ACADEMIC EXCELLENCE",
+      desc: "By surrounding yourself with like-minded individuals, you will be able to engage in a supportive and motivating environment. Our GPA requirement of 3.2 helps maintain our professional reputation while encouraging members to sustain strong academic performance. We want to recognize members and reward academic excellence while inspiring all members to achieve their best.",
+      className:
+        " flex flex-col bg-bap-brown-100 bg-opacity-50 text-white hover:bg-bap-brown-100 align-items-center",
+      img: diploma,
+    },
+    {
+      id: "PERSONAL DEVELOPMENT",
+      desc: "We prioritize personal development as it forms the foundation for professional success. The opportunities we offer will help you refine your networking, communication, and leadership skills.",
+      className:
+        "flex flex-col bg-bap-brown-100 bg-opacity-50 text-white hover:bg-bap-brown-100 align-items-center",
+      img: thinking,
+    },
+    {
+      id: "PROFESSIONAL DEVELOPMENT",
+      desc: "At BAP, we facilitate events with firms for our members to build meaningful professional relationships with recruiters and professionals. Our strong alumni network also offers valuable support and guidance throughout your college and career journey. ",
+      className:
+        "flex flex-col bg-bap-brown-100 bg-opacity-50 text-white hover:bg-bap-brown-100 align-items-center",
+      img: handshake,
+    },
+    {
+      id: "SENSE OF COMMUNITY",
+      desc: " BAP promotes participation in community service activities to give back to the community, helping members develop a strong sense of ethical, social, and public responsibility.",
+      className:
+        " flex flex-col bg-bap-brown-100 bg-opacity-50 text-white hover:bg-bap-brown-100 align-items-center",
+      img: discussion,
+    },
+  ];
+
+  const handleClick = (id) => {
+    setActive(id);
+  };
+
   return (
     <div className="flex w-10/12 flex-col gap-10 p-20">
       <p className="text-5xl">OUR PURPOSE</p>
@@ -15,94 +58,31 @@ const Purpose = () => {
         members.
       </p>
 
-      <div className="flex flex-row place-items-center justify-center overflow-hidden bg-bap-brown-100 bg-opacity-50 p-3">
-        <div
-          id="academic"
-          className="group/academic relative flex flex-col items-center justify-center px-3 hover:bg-bap-brown-100"
-        >
-          <Image alt="diploma" src={diploma} className="l-40 w-40"></Image>
-          <span className="text-center text-2xl">
-            ACADEMIC <br />
-            EXCELLENCE
-          </span>
+      <div className="align-items-center flex w-full flex-col">
+        <div className="flex w-full flex-row">
+          {items.map((item) => (
+            <button
+              key={item.id}
+              onClick={() => setActive(item.id)}
+              className={`${item.className} px-4 py-10 text-2xl`}
+            >
+              <Image
+                src={item.img}
+                alt={item.id}
+                className="l-40 mb-3 flex w-40 flex-row"
+              />
 
-          <div className="hidden flex-row bg-bap-red-200 group-hover/academic:flex group-hover/academic:bg-bap-red-200">
-            <ul>
-              <li>
-                <a className="block">
-                  By surrounding yourself with like-minded individuals, you will
-                  be able to engage in a supportive and motivating environment.
-                  Our GPA requirement of 3.2 helps maintain our professional
-                  reputation while encouraging members to sustain strong
-                  academic performance. We want to recognize members and reward
-                  academic excellence while inspiring all members to achieve
-                  their best.
-                </a>
-              </li>
-            </ul>
-          </div>
+              {item.id}
+            </button>
+          ))}
         </div>
 
-        <div
-          id="personal"
-          className="group/personal relative flex flex-col items-center justify-center px-3 hover:bg-bap-brown-100"
-        >
-          <Image alt="thinking" src={thinking} className="l-40 w-40"></Image>
-          <span className="text-center text-2xl">
-            PERSONAL <br />
-            DEVELOPMENT
-          </span>
-
-          <div className="hidden flex-col group-hover/personal:flex group-hover/personal:bg-bap-red-200">
-            <a className="block">
-              We prioritize personal development as it forms the foundation for
-              professional success. The opportunities we offer will help you
-              refine your networking, communication, and leadership skills.
-            </a>
-          </div>
-        </div>
-
-        <div
-          id="prof"
-          className="group/prof relative flex flex-col items-center justify-center px-3 hover:bg-bap-brown-100"
-        >
-          <Image alt="handshake" src={handshake} className="l-40 w-40"></Image>
-          <span className="text-center text-2xl">
-            PROFESSIONAL <br />
-            DEVELOPMENT
-          </span>
-
-          <div className="hidden flex-col group-hover/prof:flex group-hover/prof:bg-bap-red-200">
-            <a className="block">
-              At BAP, we facilitate events with firms for our members to build
-              meaningful professional relationships with recruiters and
-              professionals. Our strong alumni network also offers valuable
-              support and guidance throughout your college and career journey.
-            </a>
-          </div>
-        </div>
-
-        <div
-          id="sense"
-          className="group/sense relative flex flex-col items-center justify-center px-3 hover:bg-bap-brown-100"
-        >
-          <Image
-            alt="discussion"
-            src={discussion}
-            className="l-40 w-40"
-          ></Image>
-          <span className="text-center text-2xl">
-            SENSE OF <br />
-            COMMUNITY
-          </span>
-
-          <div className="hidden flex-col group-hover/sense:flex group-hover/sense:bg-bap-red-200">
-            <a className="block">
-              BAP promotes participation in community service activities to give
-              back to the community, helping members develop a strong sense of
-              ethical, social, and public responsibility.
-            </a>
-          </div>
+        <div className="description w-full">
+          {active && (
+            <p className="flex flex-row bg-bap-red-200 p-2">
+              {items.find((item) => item.id === active)?.desc}
+            </p>
+          )}
         </div>
       </div>
     </div>
