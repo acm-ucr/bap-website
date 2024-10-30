317a7f77f0bd9e59b496f4837f4f4b2d7811d9f7
Jonathan Le <jonathanle062303@gmail.com>
2024-10-29 21:03:01 -0700

    Arrange about page

--- src/components/about/Purpose.tsx
+++ src/components/about/Purpose.tsx
@@ -6,7 +6,7 @@ import discussion from "@/public/discussion.webp";
 
 const Purpose = () => {
   return (
-    <div className="flex w-10/12 flex-col gap-10 p-20">
+    <div className="flex w-full flex-col gap-10">
       <p className="text-5xl">OUR PURPOSE</p>
 
       <p className="text-2xl font-light">
@@ -15,41 +15,47 @@ const Purpose = () => {
         members.
       </p>
 
-      <div className="flex flex-row place-items-center bg-bap-brown-100 bg-opacity-50 p-3">
-        <div className="flex flex-col items-center justify-center px-3">
-          <Image alt="diploma" src={diploma} className="l-40 w-40"></Image>
-          <span className="text-center text-2xl">
-            ACADEMIC <br />
-            EXCELLENCE
-          </span>
-        </div>
+      <div className="flex w-full justify-center">
+        <div className="flex w-10/12 flex-row place-items-center justify-between bg-bap-brown-100 bg-opacity-50 p-3">
+          <div className="flex flex-col items-center justify-center px-3">
+            <Image alt="diploma" src={diploma} className="l-40 w-40"></Image>
+            <span className="text-center text-2xl">
+              ACADEMIC <br />
+              EXCELLENCE
+            </span>
+          </div>
 
-        <div className="flex flex-col items-center justify-center px-3">
-          <Image alt="thinking" src={thinking} className="l-40 w-40"></Image>
-          <span className="text-center text-2xl">
-            PERSONAL <br />
-            DEVELOPMENT
-          </span>
-        </div>
+          <div className="flex flex-col items-center justify-center px-3">
+            <Image alt="thinking" src={thinking} className="l-40 w-40"></Image>
+            <span className="text-center text-2xl">
+              PERSONAL <br />
+              DEVELOPMENT
+            </span>
+          </div>
 
-        <div className="flex flex-col items-center justify-center px-3">
-          <Image alt="handshake" src={handshake} className="l-40 w-40"></Image>
-          <span className="text-center text-2xl">
-            PROFESSIONAL <br />
-            DEVELOPMENT
-          </span>
-        </div>
+          <div className="flex flex-col items-center justify-center px-3">
+            <Image
+              alt="handshake"
+              src={handshake}
+              className="l-40 w-40"
+            ></Image>
+            <span className="text-center text-2xl">
+              PROFESSIONAL <br />
+              DEVELOPMENT
+            </span>
+          </div>
 
-        <div className="flex flex-col items-center justify-center px-3">
-          <Image
-            alt="discussion"
-            src={discussion}
-            className="l-40 w-40"
-          ></Image>
-          <span className="text-center text-2xl">
-            SENSE OF <br />
-            COMMUNITY
-          </span>
+          <div className="flex flex-col items-center justify-center px-3">
+            <Image
+              alt="discussion"
+              src={discussion}
+              className="l-40 w-40"
+            ></Image>
+            <span className="text-center text-2xl">
+              SENSE OF <br />
+              COMMUNITY
+            </span>
+          </div>
         </div>
       </div>
     </div>
