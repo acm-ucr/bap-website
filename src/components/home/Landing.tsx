3afca2fbcfcec2a6c9bea0968824613247fb9d76
Aaron-Argueta <aarona2600@gmail.com>
2024-11-04 19:00:24 -0800

    Add background image to landing

--- src/components/home/Landing.tsx
+++ src/components/home/Landing.tsx
@@ -1,16 +1,19 @@
 const Landing = () => {
   return (
-    <div className="flex w-[50%] flex-col bg-bap-brown-100 bg-opacity-50 pb-7 pl-12 pr-14 pt-10">
-      <p className="text-2xl font-thin leading-8">
-        Honors Organization for Financial Information Students and Professionals
-        at UCR
-      </p>
-
-      <div className="flex w-full flex-row justify-end">
-        <p className="mt-1 w-[65%] text-sm font-thin leading-tight">
-          Empowering Tomorrow's Leaders in Finance and Business Through
-          Excellence, Ethics, and Engagement
-        </p>
+    <div className="h-[700px] w-full bg-[url('/clubimage1.webp')] bg-center bg-no-repeat">
+      <div className="h-[700px] w-full bg-bap-brown-400 bg-opacity-[93%]">
+        <div className="flex w-[50%] flex-col bg-bap-brown-100 bg-opacity-50 pb-7 pl-12 pr-14 pt-10">
+          <p className="text-2xl font-thin leading-8">
+            Honors Organization for Financial Information Students and
+            Professionals at UCR
+          </p>
+          <div className="flex w-full flex-row justify-end">
+            <p className="mt-1 w-[65%] text-sm font-thin leading-tight">
+              Empowering Tomorrow's Leaders in Finance and Business Through
+              Excellence, Ethics, and Engagement
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
