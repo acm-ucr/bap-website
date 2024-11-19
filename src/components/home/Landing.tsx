c52dee019d0ad0c4503c70ed09e97e9ee80425ef
Aaron-Argueta <aarona2600@gmail.com>
2024-11-18 20:33:28 -0800

    resolved conflicts

--- src/components/home/Landing.tsx
+++ src/components/home/Landing.tsx
@@ -2,31 +2,33 @@ import Link from "next/link";
 
 const Landing = () => {
   return (
-    <div className="flex w-[50%] flex-col bg-bap-brown-100 bg-opacity-50 pb-7 pl-12 pr-14 pt-10">
-        <div className="absolute left-0 top-0 z-[-1] h-[700px] w-full bg-[url('/clubimage1.webp')] bg-center bg-no-repeat">
+    
+    <div className="flex justify-center items-center">
+      <div className="absolute left-0 top-0 z-[-1] h-[700px] w-full bg-[url('/clubimage1.webp')] bg-center bg-no-repeat">
           <div className="h-[700px] w-full bg-bap-brown-400 bg-opacity-[93%]"></div>
         </div>
       <div className="relative flex w-[50%] flex-col bg-bap-brown-100 bg-opacity-50 pb-7 pl-12 pr-14 pt-10">
-        <h1 className="absolute -left-20 -top-10 mb-4 text-7xl font-thin tracking-wide">
-          {" "}
-          BETA ALPHA PSI{" "}
-        </h1>
-        <p className="text-2xl font-thin leading-8">
-          Honors Organization for Financial Information Students and
-          Professionals at UCR
+      <h1 className="absolute -left-20 -top-10 mb-4 text-7xl font-thin tracking-wide">
+        {" "}
+        BETA ALPHA PSI{" "}
+      </h1>
+      <p className="text-2xl font-thin leading-8">
+        Honors Organization for Financial Information Students and Professionals
+        at UCR
+      </p>
+
+      <div className="flex w-full flex-row justify-end">
+        <p className="mt-1 w-[65%] text-sm font-thin leading-tight">
+          Empowering Tomorrow's Leaders in Finance and Business Through
+          Excellence, Ethics, and Engagement
         </p>
-        <div className="flex w-full flex-row justify-end">
-          <p className="mt-1 w-[65%] text-sm font-thin leading-tight">
-            Empowering Tomorrow's Leaders in Finance and Business Through
-            Excellence, Ethics, and Engagement
-          </p>
-        </div>
-        <Link href="https://docs.google.com/forms/d/e/1FAIpQLSck3Q0dWxRiUjs4VKRAwED7inrMH1NC8SbHVAgIWSHVnZW_5A/viewform">
-          <div className="absolute -bottom-5 -right-10 cursor-pointer self-center rounded bg-bap-red-200 px-4 py-2 font-semibold text-white hover:bg-red-800">
-            Join Us
-          </div>
-        </Link>
       </div>
+      <Link href="https://docs.google.com/forms/d/e/1FAIpQLSck3Q0dWxRiUjs4VKRAwED7inrMH1NC8SbHVAgIWSHVnZW_5A/viewform">
+        <div className="absolute -bottom-5 -right-10 cursor-pointer self-center rounded bg-bap-red-200 px-4 py-2 font-semibold text-white hover:bg-red-800">
+          Join Us
+        </div>
+      </Link>
+    </div>
     </div>
   );
 };
