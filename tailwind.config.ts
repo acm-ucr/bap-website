bd64eb99793f28a3f9c3a39bf0649240161aa60c
Anthony Vuong <avuon032@xe-02.cs.ucr.edu>
2024-11-21 16:23:04 -0800

    animating landing page box

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,16 @@ module.exports = {
   content: ["./src/**/*.{ts,tsx}"],
   theme: {
     extend: {
+      keyframes: {
+        "drop-in": {
+          "0%": { transform: "translateY(-20px)", opacity: "0" },
+
+          "100%": { transform: "translateY(0)", opacity: "1" },
+        },
+      },
+      animation: {
+        "drop-in": "drop-in 0.5s ease-in-out",
+      },
       colors: {
         bap: {
           "brown-100": "#C5C19D",
