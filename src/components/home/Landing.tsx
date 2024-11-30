c0447d6fd717df62e574b2487fd10a96754684f4
Jonathan Le <jonathanle062303@gmail.com>
2024-11-30 12:25:27 -0800

    Fix landing responsiveness

--- src/components/home/Landing.tsx
+++ src/components/home/Landing.tsx
@@ -6,18 +6,18 @@ const Landing = () => {
       <div className="absolute left-0 top-0 z-[-1] h-[700px] w-full bg-[url('/clubimage1.webp')] bg-center bg-no-repeat">
         <div className="h-[700px] w-full bg-bap-brown-400 bg-opacity-[93%]"></div>
       </div>
-      <div className="relative flex w-[50%] animate-fade-down flex-col bg-bap-brown-100 bg-opacity-50 pb-7 pl-12 pr-14 pt-10">
-        <h1 className="absolute -left-20 -top-10 mb-4 text-7xl font-thin tracking-wide">
+      <div className="relative flex w-5/6 animate-fade-down flex-col bg-bap-brown-100 bg-opacity-50 p-4 sm:w-[50%] sm:pb-7 sm:pl-12 sm:pr-14 sm:pt-10">
+        <h1 className="absolute -left-4 -top-5 mb-4 text-4xl font-thin sm:-left-12 sm:-top-8 sm:text-5xl sm:tracking-wide lg:-left-20 lg:-top-12 lg:text-7xl">
           {" "}
           BETA ALPHA PSI{" "}
         </h1>
-        <p className="text-2xl font-thin leading-8">
+        <p className="text-xl font-thin lg:text-2xl lg:leading-8">
           Honors Organization for Financial Information Students and
           Professionals at UCR
         </p>
 
         <div className="flex w-full flex-row justify-end">
-          <p className="mt-1 w-[65%] text-sm font-thin leading-tight">
+          <p className="mt-1 w-4/5 text-sm font-thin leading-tight sm:w-[65%]">
             Empowering Tomorrow's Leaders in Finance and Business Through
             Excellence, Ethics, and Engagement
           </p>
