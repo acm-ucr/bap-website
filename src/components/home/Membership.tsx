2de9138416274d85c4926e6037c98af35c66b5fe
daniyalbhat34 <daniyalbhat34@gmail.com>
2024-12-05 22:23:51 -0800

    fixed buttons

--- src/components/home/Membership.tsx
+++ src/components/home/Membership.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 const Membership = () => {
   return (
     <div className="w-100 flex-col bg-bap-red-200 px-10 py-20 text-center">
@@ -6,10 +8,13 @@ const Membership = () => {
         Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit, Sed Do Eiusmod
         Tempor Incididunt Ut Labore Et Dolore Magna Aliqua
       </p>
-      <button>
-        <p className="relative -bottom-6 rounded-sm bg-bap-brown-100 bg-opacity-[34%] px-12 py-3 text-center text-3xl shadow-md shadow-black">
+      <button className="relative -bottom-6 rounded-sm bg-bap-brown-100 bg-opacity-[34%] px-12 py-3 text-center text-3xl shadow-md shadow-black hover:bg-red-800 active:bg-red-600">
+        <Link
+          href="https://docs.google.com/forms/d/e/1FAIpQLSck3Q0dWxRiUjs4VKRAwED7inrMH1NC8SbHVAgIWSHVnZW_5A/viewform"
+          target="_blank"
+        >
           Join Us
-        </p>
+        </Link>
       </button>
     </div>
   );
