c17fd407667e3af13c7235e83c16788613c44688
Jonathan Le <jonathanle062303@gmail.com>
2024-12-05 20:43:10 -0800

    Integrate calendar

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,15 +1,43 @@
+"use client";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import CustomCalendar from "@/components/events/Calendar";
 import EventCard from "@/components/events/EventCard";
 import Title from "@/components/Title";
+import { Event } from "react-big-calendar";
+
+interface GoogleEvent {
+  summary: string,
+  start: { dateTime: string },
+  end: { dateTime: string },
+};
 
 const Events = () => {
-  const tempEvents = [
-    {
-      title: "Weekly Meeting",
-      start: new Date("2024-10-24T01:00:00"),
-      end: new Date("2024-10-24T03:00:00"),
-    },
-  ];
+  const [events, setEvents] = useState<Event[]>([]);
+  useEffect(() => {
+    axios
+      .get(
+        `https://www.googleapis.com/calendar/v3/calendars/${
+          process.env.NEXT_PUBLIC_CALENDAR_ID
+        }/events?key=${
+          process.env.NEXT_PUBLIC_GOOGLE_CALENDAR_API_KEY
+        }&singleEvents=true&orderBy=starttime&timeMin=${new Date(
+          new Date().getFullYear(),
+          new Date().getMonth(),
+          1,
+        ).toISOString()}&timeMax=${new Date(
+          new Date().getMonth() === 11
+            ? new Date().getFullYear() + 1
+            : new Date().getFullYear(),
+          new Date().getMonth() === 11 ? 0 : new Date().getMonth() + 1,
+          1,
+        ).toISOString()}`,
+      )
+      .then((result) => {
+        console.log(result);
+        setEvents(result.data.items.map((item: GoogleEvent): Event => ({start: new Date(item.start.dateTime), end: new Date(item.end.dateTime), title: item.summary})));
+      });
+  }, []);
 
   return (
     <div className="mb-10 flex w-screen flex-col items-center">
@@ -19,7 +47,7 @@ const Events = () => {
       <EventCard />
       <EventCard />
       <EventCard />
-      <CustomCalendar events={tempEvents} />
+      <CustomCalendar events={events} />
     </div>
   );
 };
--- src/components/events/CustomEvent.tsx
+++ src/components/events/CustomEvent.tsx
@@ -2,7 +2,7 @@ import { EventProps } from "react-big-calendar";
 
 const CustomEvent = (props: EventProps) => {
   return (
-    <div className="2xl:text-md flex justify-center rounded-md bg-bap-red-200 bg-opacity-25 text-sm font-medium text-black">
+    <div className="2xl:text-md flex justify-center rounded-md bg-bap-red-200 bg-opacity-25 text-sm font-medium text-black text-wrap p-1 text-center text-ellipsis">
       {props.event.start
         ? props.event.start.toLocaleTimeString(navigator.language, {
             hour: "numeric",
