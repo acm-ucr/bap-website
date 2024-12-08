159865bc7b3318dd081f5e7357b3c6ca71f67b19
Jonathan Le <jonathanle062303@gmail.com>
2024-12-08 01:10:38 -0800

    Fix button and navigation

--- src/components/home/Landing.tsx
+++ src/components/home/Landing.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const Landing = () => {
   return (
-    <div className="flex items-center justify-center">
+    <div className="flex w-full items-center justify-center">
       <div className="absolute left-0 top-0 z-[-1] h-[700px] w-full bg-[url('/clubimage1.webp')] bg-center bg-no-repeat">
         <div className="h-[700px] w-full bg-bap-brown-400 bg-opacity-[93%]"></div>
       </div>
@@ -23,7 +23,7 @@ const Landing = () => {
           </p>
         </div>
         <Link href="https://docs.google.com/forms/d/e/1FAIpQLSck3Q0dWxRiUjs4VKRAwED7inrMH1NC8SbHVAgIWSHVnZW_5A/viewform">
-          <div className="absolute -bottom-5 -right-10 cursor-pointer self-center rounded bg-bap-red-200 px-4 py-2 font-semibold text-white hover:bg-red-800">
+          <div className="absolute -bottom-5 -right-4 cursor-pointer self-center rounded bg-bap-red-200 px-4 py-2 font-semibold text-white hover:bg-red-800 md:-right-10">
             Join Us
           </div>
         </Link>
