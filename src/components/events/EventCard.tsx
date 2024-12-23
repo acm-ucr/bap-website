f567a7c1166b2754f1f37d9efe8b11b6a1506192
theo <theobfernandez@gmail.com>
2024-12-23 23:28:40 +0100

    change event size

--- src/components/events/EventCard.tsx
+++ src/components/events/EventCard.tsx
@@ -8,11 +8,11 @@ const EventCard = ({ event }: EventCardProps) => {
   console.log(event);
   return (
     <div className="mx-4 my-6 flex h-[25%] w-[65%] flex-col justify-between rounded-md bg-bap-brown-100 bg-opacity-50 py-7 pl-10 sm:mx-8 md:mx-12 lg:mx-24">
-      <p className="text-3x1 w-[70%] sm:text-4xl lg:text-5xl">
+      <p className="text-3x1 w-[70%] sm:text-2xl lg:text-3xl">
         {event.title?.toLocaleString()}
       </p>
       {event.start?.toLocaleString() === "Invalid Date" ? null : (
-        <p className="mb-1 mt-4 w-[80%] rounded-xl bg-bap-red-200 p-1 text-xl font-semibold sm:text-2xl">
+        <p className="mb-1 mt-4 w-[80%] rounded-xl bg-bap-red-200 p-1 text-lg font-semibold sm:text-xl">
           {event.start?.toLocaleString()}
         </p>
       )}
